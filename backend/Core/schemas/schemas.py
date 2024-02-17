58c3757ed3b67c3865e8d905f8d3d9d3e1838327
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-17 13:04:43 +0300

    refactor: reformat

--- backend/Core/schemas/schemas.py
+++ backend/Core/schemas/schemas.py
@@ -1,35 +1,39 @@
-from typing import Annotated, List
+from typing import List
 from pydantic import BaseModel
+
 
 class Like(BaseModel):
     id: int
-    user_id:int
+    user_id: int
     post_id: int
 
+
 class Comment(BaseModel):
-    id:int
+    id: int
     text: str
     author_id: int
-    author_username:str
+    author_username: str
     post_id: int
+
 
 class Post(BaseModel):
     id: int
-    title:str
+    title: str
     previewText: str
     fullText: str
     author_id: int
     comments: List[Comment]
     likes: List[Like]
 
+
 class UserBase(BaseModel):
     username: str
+
 
 class UserCreate(UserBase):
     password: str
 
+
 class UserResponce(UserBase):
     id: int
     posts: List[Post]
-
-
