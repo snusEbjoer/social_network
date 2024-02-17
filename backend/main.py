2578c2af66835fefb56da0db3a44bfde4cd1fd32
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-17 13:06:34 +0300

    refactor: deez...

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,66 @@
-from fastapi import Depends, FastAPI, APIRouter, Request
+import sys
+from fastapi import FastAPI, APIRouter, Request
 import uvicorn
 from dotenv import load_dotenv
 from fastapi_sqlalchemy import DBSessionMiddleware
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-import os,sys
-from Core.models.database import get_db
+import os
+from Core.models.database import SessionLocal
 from routers import user
 from Core.models.database import User
 from jose import jwt
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+
+if DATABASE_URL is None or SECRET_KEY is None or ALGORITHM is None:
+    sys.stderr.write("You should provide all variables in .env file")
+    exit(1)
 
 app = FastAPI()
 router = APIRouter()
 app.include_router(user.router)
-# to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url=DATABASE_URL)
 
 
+def extract_token(header_string: str) -> str | None:
+    parts = header_string.split(" ")
+    if len(parts) != 2:
+        return
+    prefix, token = parts
+    if prefix != "Bearer":
+        return
+    return token
+
+
 @app.middleware("http")
-async def app_auth_middleware(request:Request, call_next):
-    db = get_db()
-    header = request.headers.get("Authorization")
-    if header and header.startswith("Bearer "):
-        to_decode = header.removeprefix("Bearer ")
-        user = jwt.decode(to_decode, key=SECRET_KEY, algorithms=ALGORITHM) # type: ignore
-        username: str = user.get("username") # type: ignore
-        db_user = await db.execute(select(User).where(User.username == username))
-        request.scope["user"] = db_user.scalars().one()
-        return call_next(request)
+async def app_auth_middleware(request: Request, call_next):
+    db = SessionLocal()
+    try:
+        header = request.headers.get("Authorization")
+        if header is None:
+            return await call_next()
+
+        token = extract_token(header)
+        if token is None:
+            return await call_next()
+
+        payload = jwt.decode(token, key=SECRET_KEY, algorithms=ALGORITHM)
+        db_user = await db.execute(
+            select(User).where(User.username == payload["username"])
+        )
+        request.scope["user"] = db_user.scalars().one_or_none()
+        return await call_next(request)
+    finally:
+        await db.close()
 
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run(app, port=8000, reload=True)
