5ab1c7d4873beaea7e9e8d54898cd04d6e2bc48e
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-17 13:03:35 +0300

    fix: add pass to empty function

--- backend/Core/crud/post_crud.py
+++ backend/Core/crud/post_crud.py
@@ -1,7 +1,5 @@
-from typing import NoReturn
-from Core.schemas.schemas import UserBase, UserCreate, UserResponce
-from Core.models.database import Post
 from sqlalchemy.orm import Session
 
-def create_post(token:str ,text:str, db:Session):
-  
+
+def create_post(token: str, text: str, db: Session):
+    pass
